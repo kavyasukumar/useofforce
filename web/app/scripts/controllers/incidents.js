0288f988d0a2aea0951fc01f622eeff4b1d0db52
KavyaSukumar <Kavya.Sukumar@hotmail.com>
2015-03-27 17:42:51 -0700

    Adding new controller from yeoman

---
+++ web/app/scripts/controllers/incidents.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name webApp.controller:IncidentsCtrl
+ * @description
+ * # IncidentsCtrl
+ * Controller of the webApp
+ */
+angular.module('webApp')
+  .controller('IncidentsCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
